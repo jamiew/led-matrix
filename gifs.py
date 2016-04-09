e42deaaf982c8c567b620333f98e8020bf3e445e
Jamie Wilkinson <jamie@jamiedubs.com>
2016-04-08 20:59:48 -0400

    More images. Read from directory

--- gifs.py
+++ gifs.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python
- 
+
 # http://effbot.org/imagingbook/image.htm
 # http://effbot.org/imagingbook/imagedraw.htm
- 
+
 import Image
 import ImageDraw
 import time
+import os
+import glob
+import random
 from rgbmatrix import Adafruit_RGBmatrix
- 
+
 # Rows and chain length are both required parameters:
 matrix = Adafruit_RGBmatrix(32, 1)
 
-# filename = "corgi.gif"
-# filename = "togepi.gif"
-# filename = "moogle.gif"
-# filename = "raising.gif"
-# filename = "duck.gif"
-# filename = "mainframe.gif" # TODO need to resize to 32x32
-# filename = "vhx-irondavy-smaller.gif"
-
-filenames = ['corgi', 'togepi', 'moogle', 'raisin', 'duck']
+filenames = glob.glob("img/*.gif")
+random.shuffle(filenames)
 
 file_index = 0
+# size = 32, 32
+# image.thumbnail() not behaving - dynamic resizing
 
 while True:
 	filename = filenames[file_index % len(filenames)]
 	print "Looping " + filename
-	image = Image.open("img/" + filename + ".gif")	
+	image = Image.open(filename)
 
 	loops = 0
 	frame = 0
-	
+
 	while loops < 5:
 		print "Frame: " + str(frame)
 		try:
@@ -41,7 +39,7 @@ while True:
 			loops += 1
 			print 'EOFError ' + filename + ' loops = ' + str(loops)
 			continue
-		
+
 		image.load()
 		matrix.SetImage(image.im.id, 0, 0)
 		time.sleep(0.1)
