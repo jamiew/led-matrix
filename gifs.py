aa7afa1c245a5991d1a04384bc55bbebfd744245
Jamie Wilkinson <jamie@jamiedubs.com>
2016-11-18 21:20:06 -0500

    Read from argv

--- gifs.py
+++ gifs.py
@@ -6,6 +6,7 @@
 import Image
 import ImageDraw
 import time
+import sys
 import os
 import glob
 import random
@@ -14,12 +15,20 @@ from rgbmatrix import Adafruit_RGBmatrix
 # Rows and chain length are both required parameters:
 matrix = Adafruit_RGBmatrix(32, 1)
 
-filenames = glob.glob("img/*.gif")
+if len(sys.argv) > 1:
+	gifs_dir = sys.argv[1]
+else:
+	gifs_dir = "img5"
+
+dir = os.path.dirname(os.path.realpath(__file__)) + "/" + gifs_dir
+print dir
+filenames = glob.glob(dir + "/*.gif")
 random.shuffle(filenames)
 
 file_index = 0
 # size = 32, 32
 # image.thumbnail() not behaving - dynamic resizing
+speed = 0.12
 
 while True:
 	filename = filenames[file_index % len(filenames)]
@@ -28,6 +37,8 @@ while True:
 
 	loops = 0
 	frame = 0
+
+	matrix.Fill(0x000000)
 
 	while loops < 5:
 		print "Frame: " + str(frame)
@@ -41,8 +52,9 @@ while True:
 			continue
 
 		image.load()
+		print image.im.id
 		matrix.SetImage(image.im.id, 0, 0)
-		time.sleep(0.1)
+		time.sleep(speed)
 
 	file_index += 1
 
